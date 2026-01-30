5e49d47d1da3a4c4a49fd104268ed42564b9b2a5
packjs0313 <147272391+packjs0313@users.noreply.github.com>
2026-01-31 02:43:55 +0900

    프로젝트 최신 상태 반영 (API + 프론트 연동)

--- app/main.py
+++ app/main.py
@@ -12,8 +12,10 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:4321",
         "http://localhost:8000",
+        "http://localhost:5500",
         "http://127.0.0.1:4321",
         "http://127.0.0.1:8000",
+        "http://127.0.0.1:5500",
         "https://api.mono-log.fun",
         "https://mono-log.fun",
     ],
